94ff015e44b93918e5a304d66ffc4c1cfa9d974e
Jorgernunes <jorgernunes@gmail.com>
2022-05-15 13:20:01 -0300

    Arquivos do projeto atualizados

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -17,153 +17,138 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo(altura,largura) {
+function calculaAreaRetangulo() {
   // implemente sua lógica aqui
-  altura = prompt("Digite a altura do retângulo");
-  largura = prompt("Digite a largura do retâmgulo");
-  const Altura = Number(altura);
-  const Largura = Number(largura);
-  const resultado = Altura * Largura;
-  return (console.log(resultado));
+  const altura = Number(prompt("Qual a altura do retângulo ?"));
+  const largura = Number(prompt("Qual a largura do retângulo ?"));
+
+  console.log(altura*largura);
 
 }
 
-
 // EXERCÍCIO 02
-function imprimeIdade(anoAtual,anoNascimento) {
+function imprimeIdade() {
   // implemente sua lógica aqui
-  anoAtual  = Number(prompt("Digite o ano atual"));
-  anoNascimento = Number(prompt("Digite o ano do seu nascimento"));
+  const anoAtual = Number(prompt("Digite o anos atual."));
+  const anoNascimento = Number(prompt("Digite o anos que você nasceu."));
 
-  return (console.log(anoAtual - anoNascimento));
-  
+  console.log(anoAtual-anoNascimento);
 
 }
 
 // EXERCÍCIO 03
-function calculaIMC(peso,altura) {
+function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-  peso = Number(prompt ("Digite o seu peso."));
-  altura = Number(prompt("Digite a sua altura."));
+  peso = Number(prompt("Qual o seu peso?"));
+  altura = Number(prompt("Qual a sua altura?"));
   const resultado = peso / (altura*altura);
-  const imcFinal = resultado.toFixed(1);
-  
 
-  return (console.log(imcFinal));
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
-  
+  return resultado.toFixed(1);
 
 }
 
 // EXERCÍCIO 04
-function imprimeInformacoesUsuario(nome,idade,email) {
+function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
-  nome = prompt("Qual o seu nome");
-  idade = prompt("Qual a sua idade.");
-  email = prompt ("Qual o seu eamil.");
-  
-  return (console.log("Meu nome é " + nome + 
-  ", tenho "  + idade+ " anos, e o meu email é "
-  + email + "."))
-  
+  const nome = prompt("Qual o seu nome?");
+  const idade = prompt("Qual a sua idade?");
+  const email = prompt("Qual o seu email?")
+
+  console.log("Meu nome é " + nome + ", tenho " + idade + " anos, e o meu email é "+email+".");
 
 }
 
 // EXERCÍCIO 05
-function imprimeTresCoresFavoritas(cor1,cor2,cor3) {
+function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-  cor1 = prompt("Digite sua cor favorita.");
-  cor2 = prompt("Digite outra cor favorita.");
-  cor3 = prompt("Digite mais uma cor favorita.");
+  const cor1 = prompt("Digite a sua cor favorita.");
+  const cor2 = prompt("Digite outra cor favorita.");
+  const cor3 = prompt("Digite mais uma cor favorita.");
   const cores = [cor1,cor2,cor3];
+  
+  console.log(cores);
 
-  return (console.log(cores));
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-  string =prompt("Digite uma frase.");
+  string = prompt("Digite uma frase.");
 
-  return (console.log(string.toUpperCase()));
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
-  
+  return string.toUpperCase();
+
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
-  custo  = Number(prompt("Qual o custo do teatro."));
-  valorIngresso = Number(prompt("Qual o valor do ingresso."));
+  custo = prompt("Qual o valor do teatro?");
+  valorIngresso = prompt("Qual o valor do ingresso?");
 
-  return (console.log(custo / valorIngresso));
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
-
+  return custo/valorIngresso;
 
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-  string1 =prompt("Digite uma palavra.");
-  string2 =prompt("Digite outra palavra.");
-  const tamanho1 = string1.length;
-  const tamanho2 = string2.length;
-
-  return (console.log(tamanho1 === tamanho2))
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
-
+  string1 = prompt("Digite uma palavra.");
+  string2 = prompt("Digite outra palavra.");
+ 
+  return string1.lenght === string1.lenght;
+  
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
+  array [0] = prompt("Digite uma palavra");
+  array [1] = prompt("Digite outra palavra");
+  array [2] = prompt("Digite mais uma palavra");
 
-  array[0] = prompt("Digite uma palavra.");
-  array[1] = prompt("Digite outra palavra.");
-  array[2] = prompt("Digite mais uma palavra.");
-
-  return (console.log(array[0]));
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
+  return array[0];
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-  array[0] = prompt("Digite uma palavra.");
-  array[1] = prompt("Digite outra palavra.");
-  array[2] = prompt("Digite mais uma palavra.");
-  const ultimo = array.length -1 ;
+  array [0] = prompt("Digite uma palavra");
+  array [1] = prompt("Digite outra palavra");
+  array [2] = prompt("Digite mais uma palavra");
 
-  return (console.log(array[ultimo]));
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
+  return array[2];
+
 }
-
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
+  array [0] = prompt("Digite uma palavra");
+  array [1] = prompt("Digite outra palavra");
+  array [2] = prompt("Digite mais uma palavra");
 
+  array[4] = array[3];
+  array[3] = array[0];
+  array[0] = array[4];
+
+  return array;
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-  string1  =prompt("Digite uma palavra.");
-  string2  =prompt("Digite outra palavra.");
-  const palavra1 = string1.toUpperCase();
-  const palavra2 = string2.toUpperCase();
+  string1 = prompt("Digite uma palavra.");
+  string2 = prompt("Digite outra palavra.");
 
-  return ((console.log(palavra1 === palavra2)));
-  //Fiz o teste no console e funcionou mas na correção automatica esta dado erro.
+  return string1.toUpperCase() === string2.toUpperCase();
 
 }
+//################ O meu navegador estava bugando a correção automatica, fiz pelo console.
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
-
 
 }
 
