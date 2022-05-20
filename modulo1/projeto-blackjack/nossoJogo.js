7b628af9d1af45f94bbcc0d795ca92448097d0be
Jorgernunes <jorgernunes@gmail.com>
2022-05-20 19:05:17 -0300

    Projeto Blackjack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,18 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+    if(confirm("Quer iniciar uma nova rodada?")) {
+      // o que fazer se o usuário clicar "ok"
+   } else {
+      // o que fazer se o usuário clicar "cancelar"
+      console.log("O jogo acabou")
+   }
